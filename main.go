94542233a765f6dfc21ae499570a0795fbd13d81
evlbit <evlbit@proton.me>
2025-01-26 10:16:33 +0200

    Improved logging

--- main.go
+++ main.go
@@ -5,53 +5,50 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
 
 func main() {
+	log.SetPrefix("jwt-dump: ")
+	log.SetFlags(log.LstdFlags &^ (log.Ldate | log.Ltime))
+
 	args := os.Args
 
 	if len(args) < 2 {
-		fmt.Println("token not provided")
-		return
+		log.Fatalln("token not provided")
 	} else if len(args) > 2 {
-		fmt.Println("too many arguments")
-		return
+		log.Fatalln("too many arguments")
 	}
 
 	token := args[1]
 	tokenParts := strings.Split(token, ".")
 
 	if len(tokenParts) != 3 {
-		fmt.Println("invalid token")
-		return
+		log.Fatalln("invalid token")
 	}
 
 	header, err := base64.RawURLEncoding.DecodeString(tokenParts[0])
 	if err != nil {
-		fmt.Println("invalid token")
-		return
+		log.Fatalln("could not decode header")
 	}
 
 	payload, err := base64.RawURLEncoding.DecodeString(tokenParts[1])
 	if err != nil {
-		fmt.Println("invalid token")
-		return
+		log.Fatalln("could not decode payload")
 	}
 
 	var prettyHeader bytes.Buffer
 	err = json.Indent(&prettyHeader, header, "", "  ")
 	if err != nil {
-		fmt.Println("invalid token")
-		return
+		log.Fatalln("invalid header json")
 	}
 
 	var prettyPayload bytes.Buffer
 	err = json.Indent(&prettyPayload, payload, "", "  ")
 	if err != nil {
-		fmt.Println("invalid token")
-		return
+		log.Fatalln("invalid payload json")
 	}
 
 	fmt.Println()
